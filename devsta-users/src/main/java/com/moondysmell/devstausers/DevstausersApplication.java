9a0e4683e2c241284d07ba53af15cfc06b9d75b3
smellhyang <dlgiddk10@gmail.com>
2022-07-05 03:43:49 +0900

    feat/config/issue2

--- devsta-users/src/main/java/com/moondysmell/devstausers/DevstausersApplication.java
+++ devsta-users/src/main/java/com/moondysmell/devstausers/DevstausersApplication.java
@@ -3,9 +3,17 @@ package com.moondysmell.devstausers;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class DevstausersApplication {
+
+	@RequestMapping("/")
+	String home() {
+		return "Hello World!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DevstausersApplication.class, args);
