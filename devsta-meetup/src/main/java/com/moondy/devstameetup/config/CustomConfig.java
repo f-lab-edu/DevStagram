d162448c1df7af30d2f016baf73b084a09758f6c
smellhyang <dlgiddk10@gmail.com>
2022-07-05 02:14:02 +0900

    feat/config/issue2

---
+++ devsta-meetup/src/main/java/com/moondy/devstameetup/config/CustomConfig.java
@@ -0,0 +1,17 @@
+package com.moondy.devstameetup.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.stereotype.Component;
+
+@RefreshScope
+@Component
+public class CustomConfig {
+
+    @Value("${my-config.name}")
+    private  String configName;
+
+    public String getConfigName(){
+        return configName;
+    }
+}
--- devsta-users/src/main/java/com/moondysmell/devstausers/DevstausersApplication.java
+++ devsta-users/src/main/java/com/moondysmell/devstausers/DevstausersApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
-@RefreshScope //서비스 개발시 모든 주소를 통해가야하기 때문에 controller로 옮겨줄 예정
 public class DevstausersApplication {
 
 	public static void main(String[] args) {
