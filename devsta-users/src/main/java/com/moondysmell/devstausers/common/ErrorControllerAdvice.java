adceb6457a35badd48918ccdb772361f352fe195
dayoungMM <dalbeetm@gmail.com>
2022-07-15 01:30:39 +0900

    feat: NoSuchElementException 에러 핸들러

--- devsta-users/src/main/java/com/moondysmell/devstausers/common/ErrorControllerAdvice.java
+++ devsta-users/src/main/java/com/moondysmell/devstausers/common/ErrorControllerAdvice.java
@@ -1,16 +1,18 @@
 package com.moondysmell.devstausers.common;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.NoSuchElementException;
 
 @RestControllerAdvice
 @Slf4j
 public class ErrorControllerAdvice {
 
     @ExceptionHandler(value = CustomException.class)
-//    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
     protected ResponseEntity<CommonResponse<String>> handleCustomException(CustomException e) {
 //        ErrorResponse response = ErrorResponse.of(e.getErrorCode());
         CommonResponse<String> response= new CommonResponse(e.getErrorCode());
@@ -19,5 +21,12 @@ public class ErrorControllerAdvice {
         log.error(">>> " + response);
         log.error(e.getMessage());
         return new ResponseEntity<>(response, e.getHttpStatus());
+    }
+
+    @ExceptionHandler(value = NoSuchElementException.class)
+    protected ResponseEntity<CommonResponse<String>> handleNoSuchElementException(Exception e) {
+        CommonResponse<String> response= new CommonResponse(CommonCode.FAIL);
+        log.error(e.getMessage());
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 }
