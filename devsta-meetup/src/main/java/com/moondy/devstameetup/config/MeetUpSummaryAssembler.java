c3d3f157f8c6d8af7cf2188d0e33485c5144d0ef
dayoungMM <dalbeetm@gmail.com>
2022-07-29 22:07:52 +0900

    [meetup] read/service api 분리 (토큰 필요 유무로 분리) - gateway에서 토큰 인증 필터 적용하기 위해

--- devsta-meetup/src/main/java/com/moondy/devstameetup/config/MeetUpSummaryAssembler.java
+++ devsta-meetup/src/main/java/com/moondy/devstameetup/config/MeetUpSummaryAssembler.java
@@ -1,9 +1,9 @@
 package com.moondy.devstameetup.config;
 
-import com.moondy.devstameetup.controller.MeetUpController;
+import com.moondy.devstameetup.controller.MeetUpReadController;
+import com.moondy.devstameetup.controller.MeetUpSvcController;
 import com.moondy.devstameetup.domain.document.MeetUp;
 import com.moondy.devstameetup.domain.dto.MeetUpSummaryDto;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.Link;
@@ -21,7 +21,7 @@ public class MeetUpSummaryAssembler implements RepresentationModelAssembler<Meet
 
     @Override
     public EntityModel<MeetUpSummaryDto> toModel(MeetUpSummaryDto entity) {
-        return EntityModel.of(entity, linkTo(methodOn(MeetUpController.class).getOneMeetUp(entity.getId())).withSelfRel());
+        return EntityModel.of(entity, linkTo(methodOn(MeetUpReadController.class).getOneMeetUp(entity.getId())).withSelfRel());
     }
 
     public CollectionModel<EntityModel<MeetUpSummaryDto>> toCollection(List<MeetUp> entityList, Link nextLink) {
@@ -30,7 +30,7 @@ public class MeetUpSummaryAssembler implements RepresentationModelAssembler<Meet
                 = entityList.stream().map(meetUp -> {
             return EntityModel.of(meetUp.toSummaryDto(),
                     // 각 엔티티 모델마다 링크 추가.
-                    linkTo(methodOn(MeetUpController.class).getOneMeetUp(meetUp.getId().toString())).withSelfRel());
+                    linkTo(methodOn(MeetUpReadController.class).getOneMeetUp(meetUp.getId().toString())).withSelfRel());
         }).collect(Collectors.toList());
 
         // 엔티티 모델들과 별개로 listAll 메서드 링크 추가.
