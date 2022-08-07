89cde3f5ce1dc49e9f418d41376bc64ecd580d3b
dayoungMM <dalbeetm@gmail.com>
2022-08-07 19:18:15 +0900

    [meetup] feat: DTO 유효성 검사

--- devsta-users/src/main/java/com/moondysmell/devstausers/domain/dto/ChangePwDto.java
+++ devsta-users/src/main/java/com/moondysmell/devstausers/domain/dto/ChangePwDto.java
@@ -2,9 +2,19 @@ package com.moondysmell.devstausers.domain.dto;
 
 import lombok.Getter;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
 @Getter
 public class ChangePwDto {
+    @NotBlank(message = "이메일은 필수 입력 값입니다.")
+    @Email(message = "이메일 형식에 맞지 않습니다.")
     private String email;
+    @NotBlank(message = "현재 비밀번호는 필수 입력 값입니다.")
     private String password;
+    @NotBlank(message = "새 비밀번호는 필수 입력 값입니다.")
+    @Pattern(regexp="(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,20}",
+            message = "비밀번호는 영문 대,소문자와 숫자, 특수기호가 적어도 1개 이상씩 포함된 8자 ~ 20자의 비밀번호여야 합니다.")
     private String newPassword;
 }
