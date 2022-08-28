88fa36db68919af5230063b68559d79cbf4ce36c
dayoungMM <dalbeetm@gmail.com>
2022-08-28 12:21:14 +0900

    [gateway]JWTFilter 에러시 CommonResponse 형식으로 리턴되도록 수정

--- devsta-gateway/src/main/java/com/moondysmell/gateway/common/CommonCode.java
+++ devsta-gateway/src/main/java/com/moondysmell/gateway/common/CommonCode.java
@@ -20,7 +20,10 @@ public enum CommonCode {
     NOT_EXIST_ID(400,-1000, "존재하지 않는 이메일입니다."),
     WRONG_PASSWORD(400, -1001, "비밀번호가 일치하지 않습니다."),
     USER_ALREADY_EXIST(400, -1002, "해당 아이디가 이미 존재합니다."),
-    NICKNAME_ALREADY_EXIT(400, -1003 , "중복된 닉네임입니다." );
+    NICKNAME_ALREADY_EXIT(400, -1003 , "중복된 닉네임입니다." ),
+    NO_AUTH_TOKEN(400, -1004, "헤더에 Authorization 토큰이 없습니다."),
+    INVALID_AUTH_TOKEN(400, -1005, "Authorization 토큰이 유효하지 않습니다.");
+
 
     //-2000: MeetUp
 
