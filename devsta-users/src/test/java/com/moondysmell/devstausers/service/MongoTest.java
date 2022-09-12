530926f04079f6072865771c3f58361394edd1ec
dayoungMM <dalbeetm@gmail.com>
2022-09-12 23:18:30 +0900

    [bug] mongoDB test 관련 Github Actions에서 에러나는 테스트 제거 - private subnet에 있는 DB를 접속하기 위해서는 Bastion 서버를 통해 Local Forward를 해서 접속해야하는데, Github Actions에서 구현이 안되어있어 에러 발생 - 추후 Issue 따서 유닛 테스트 작성 예정

--- devsta-users/src/test/java/com/moondysmell/devstausers/service/MongoTest.java
+++ devsta-users/src/test/java/com/moondysmell/devstausers/service/MongoTest.java
@@ -1,36 +1,36 @@
-package com.moondysmell.devstausers.service;
-
-import com.moondysmell.devstausers.domain.document.DevUser;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.query.Criteria;
-import org.springframework.data.mongodb.core.query.Query;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
-
-import java.util.List;
-
-@SpringJUnitConfig(TestConfig.class)
-@TestPropertySource(properties = "spring.mongodb.embedded.version=3.5.5")
-@EnableAutoConfiguration
-@DataMongoTest
-public class MongoTest {
-    @Autowired
-    @Qualifier("testMongoTemplate")
-    private MongoTemplate mongoTemplate;
-
-    @Test
-    @DisplayName("mongoTemplate Custom 빈 등록")
-    public void findOneTest() {
-        Query query = new Query(Criteria.where("email").is("momo@gmail.com"));
-        List<DevUser> target = mongoTemplate.find(query, DevUser.class);
-        Assertions.assertEquals(1, target.size());
-        Assertions.assertEquals("김모모", target.get(0).getName());
-    }
-}
+//package com.moondysmell.devstausers.service;
+//
+//import com.moondysmell.devstausers.domain.document.DevUser;
+//import org.junit.jupiter.api.Assertions;
+//import org.junit.jupiter.api.DisplayName;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Qualifier;
+//import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+//import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+//import org.springframework.data.mongodb.core.MongoTemplate;
+//import org.springframework.data.mongodb.core.query.Criteria;
+//import org.springframework.data.mongodb.core.query.Query;
+//import org.springframework.test.context.TestPropertySource;
+//import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+//
+//import java.util.List;
+//
+//@SpringJUnitConfig(TestConfig.class)
+//@TestPropertySource(properties = "spring.mongodb.embedded.version=3.5.5")
+//@EnableAutoConfiguration
+//@DataMongoTest
+//public class MongoTest {
+//    @Autowired
+//    @Qualifier("testMongoTemplate")
+//    private MongoTemplate mongoTemplate;
+//
+//    @Test
+//    @DisplayName("mongoTemplate Custom 빈 등록")
+//    public void findOneTest() {
+//        Query query = new Query(Criteria.where("email").is("momo@gmail.com"));
+//        List<DevUser> target = mongoTemplate.find(query, DevUser.class);
+//        Assertions.assertEquals(1, target.size());
+//        Assertions.assertEquals("김모모", target.get(0).getName());
+//    }
+//}
